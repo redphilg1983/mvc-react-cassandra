19ed4281636a02e87982ef467979ba57a0325862
Philip Green <philip.green@travelcounsellors.com>
2020-06-16 11:49:18 +0100

    setup cassandra scripts to create table and data

---
+++ film-tv-game/CassandraDataSetup.js
@@ -0,0 +1,79 @@
+﻿var cassandra = require('cassandra-driver');
+var client = new cassandra.Client({
+    contactPoints: ['192.168.0.40'],
+    localDataCenter: 'datacenter1'
+});
+var generator = require('node-uuid-generator');
+
+//Script to create the data to be used by the project
+
+
+client.execute(
+    "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES(" + generator.generate() + ", 'Film', 'Avengers: Endgame', 5);",
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'Film', 'Spiderman: Far From Home', 3);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'Film', 'Star Wars: The Rise of Skywalker', 2);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'TV', 'Star Trek: Enterprise', 4);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'TV', 'The Curse of oak Island', 3);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'TV', 'Prison Break', 1);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'Game', 'COD: Modern Warfare', 6);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    ).then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'Game', 'Apex Legends', 2);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+            }
+        )
+    )
+    .then(() =>
+        client.execute(
+            "INSERT INTO ekm.voting(guid, genre, title, votes) VALUES (" + generator.generate() + ", 'Game', 'Minecraft Dungeons', 1);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+                client.shutdown();
+            }
+        )
+    );
---
+++ film-tv-game/CassandraTableSetup.js
@@ -0,0 +1,24 @@
+﻿var cassandra = require('cassandra-driver');
+var client = new cassandra.Client({
+    contactPoints: ['192.168.0.40'],
+    localDataCenter: 'datacenter1'
+});
+var generator = require('node-uuid-generator');
+
+//Script to create the keyspace and the table to be used by the project
+var guid = generator.generate();
+
+client
+    .execute(
+        "CREATE KEYSPACE IF NOT EXISTS ekm WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 };"
+    )
+    .then(() =>
+        client.execute(
+            "CREATE TABLE IF NOT EXISTS ekm.voting(guid uuid primary key, genre text, title text, votes int);",
+            (err, result) => {
+                console.log(err);
+                console.log(err, result);
+                client.shutdown();
+            }
+        )
+    );
